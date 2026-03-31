df078275ed95a335b60e73fc2d59cc4c3e6eadac
CodeCrafter2001 <anmolbansiwal1@gmail.com>
2026-03-31 16:15:02 +0530

    chat layout setup

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -14,7 +14,7 @@ function LoginPage() {
     login(formData);
   };
 
-  return (
+  return (  
     <div className="w-full flex items-center justify-center p-4 bg-slate-900">
       <div className="relative w-full max-w-6xl md:h-[800px] h-[650px]">
         <BorderAnimatedContainer>
