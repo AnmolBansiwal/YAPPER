660fb78569bc060bc63cae640c5e4d886599df69
CodeCrafter2001 <anmolbansiwal1@gmail.com>
2026-04-10 15:33:12 +0530

    added sockit.io

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,24 +7,28 @@ import cookieParser from "cookie-parser";
 import path from "path";
 import { fileURLToPath } from "url";
 import { connectDB } from "./lib/db.js";
-const app = express(); 
+import { app, server } from "./lib/socket.js";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = ENV.PORT || 3000;
+
 app.use(
   cors({
     origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
   })
 );
-app.use (express.json({limit: "5mb"})); //req.body
-//routes
-app.use(cookieParser());
-app.use('/api/auth', authRoutes); 
-app.use('/api/messages',messageRoute); 
 
-// 🔥 absolute path — no relative bugs
+app.use(express.json({ limit: "5mb" }));
+app.use(cookieParser());
+
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoute);
+
+// absolute path
 const distPath = path.resolve(__dirname, "../../Frontend/dist");
 
 if (ENV.NODE_ENV === "production") {
@@ -35,12 +39,7 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-
-
-
-
-app.listen(PORT,()=>{
-    console.log("server is listening on port:" + PORT);
-    connectDB(); 
-}); 
-  
+server.listen(PORT, async () => {
+  console.log("server is listening on port: " + PORT);
+  await connectDB();
+});
